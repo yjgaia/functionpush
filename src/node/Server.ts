adde76316a73920b569052a1539de78fb1d71270
hanul <hanul@hanul.me>
2021-01-07 23:40:39 +0900

    making server

--- src/node/Server.ts
+++ src/node/Server.ts
@@ -1,2 +1,11 @@
-//TODO: web server
-//TODO: web socket server
+export default class Server {
+
+    constructor(
+        port: number, keyPath: string, certPath: string,
+        httpPort: number,
+    ) {
+
+        //TODO: web server
+        //TODO: web socket server
+    }
+}
